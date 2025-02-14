7bfb44081eb1cec0719548b4831a4c9b3432467c
beshoycoding123 <beshoy.shawky.2013@gmail.com>
2025-02-14 14:28:38 +0400

    and or operators

---
+++ AND OR.py
@@ -0,0 +1,10 @@
+a=int(input("enter a number: "))
+b=int(input("enter another number: "))
+if a>0 and b>0:
+    print("a and b are positive numbers")
+else:
+    print("a and b are negative numbers")
+if a>0 or b>0:
+    print("a and b are positive numbers")
+else:
+    print("a and b are negative numbers")
