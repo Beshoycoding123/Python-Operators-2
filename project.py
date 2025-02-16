1791335f3e795324f980b36c0b6dacc70ae7adba
beshoycoding123 <beshoy.shawky.2013@gmail.com>
2025-02-16 12:25:13 +0400

    char

---
+++ project.py
@@ -0,0 +1,5 @@
+char=input("enter a character: ")
+if char=="a"or"b"or"c"or"d"or"e"or"f"or"g"or"h"or"i"or"j"or"k"or"l"or"m"or"n"or"o"or"p"or"q"or"r"or"s"or"t"or"u"or"v"or"w"or"x"or"y"or"z":
+    print("your input is part of the alphabet")
+else:
+    print("your input is not part of the alphabet")
