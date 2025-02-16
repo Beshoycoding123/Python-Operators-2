087bd36d9b60231b650887b474ba66684671319d
beshoycoding123 <beshoy.shawky.2013@gmail.com>
2025-02-16 14:41:57 +0400

    alphabet

--- project.py
+++ project.py
@@ -1,5 +1,7 @@
 char=input("enter a character: ")
-if char=="a"or"b"or"c"or"d"or"e"or"f"or"g"or"h"or"i"or"j"or"k"or"l"or"m"or"n"or"o"or"p"or"q"or"r"or"s"or"t"or"u"or"v"or"w"or"x"or"y"or"z":
+if str(char=="a"or"b"or"c"or"d"or"e"or"f"or"g"or"h"or"i"or"j"or"k"or"l"or"m"or"n"or"o"or"p"or"q"or"r"or"s"or"t"or"u"or"v"or"w"or"x"or"y"or"z"):
     print("your input is part of the alphabet")
-else:
+elif char!="a"or"b"or"c"or"d"or"e"or"f"or"g"or"h"or"i"or"j"or"k"or"l"or"m"or"n"or"o"or"p"or"q"or"r"or"s"or"t"or"u"or"v"or"w"or"x"or"y"or"z":
     print("your input is not part of the alphabet")
+else:
+    print("input is invalid")
